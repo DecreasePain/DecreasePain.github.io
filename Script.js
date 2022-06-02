cd404ae10cee8dcb3a0419e16319b76c818d57e5
Gork <51435938+DecreasePain@users.noreply.github.com>
2022-06-01 22:30:01 -0400

    999

--- Script.js
+++ Script.js
@@ -2,6 +2,7 @@ var Data = new Date("Jun, 08, 2022, 00:00:00").getTime();
 var DataAtual = new Date()
 
 var Hora = DataAtual.getHours();
+Saudar = "Olá, Lívia!"
 if (Hora >= 0 && Hora < 12){Saudar = "Bom dia, Lívia!"}
 else if (Hora >= 12 && Hora < 17){Saudar = "Boa tarde, Lívia!"}
 else if (Hora >= 17 && Hora < 0){Saudar = "Boa noite, Lívia!"}
