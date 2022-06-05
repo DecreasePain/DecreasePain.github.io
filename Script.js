036a5df93059b699587b50808c1b7a0d5feef369
Gork <51435938+DecreasePain@users.noreply.github.com>
2022-06-05 19:23:26 -0400

    Alteração de horário

--- Script.js
+++ Script.js
@@ -1,4 +1,4 @@
-var Data = new Date("Jun, 17, 2022, 17:00:00").getTime();
+var Data = new Date("Jun, 17, 2022, 20:00:00").getTime();
 var DataAtual = new Date()
 
 function Aleatorizar(Lista){return Lista[Math.floor((Math.random()*Lista.length))]}
