d8e7573b6f10d6e55fb1c8722c7b32c098d2b856
Gork <51435938+DecreasePain@users.noreply.github.com>
2022-06-10 22:55:44 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -6,7 +6,7 @@ function Aleatorizar(Lista){return Lista[Math.floor((Math.random()*Lista.length)
 Som = document.getElementById("Plim");
 function TocarSom(){
     Som.play();
-    document.getElementById("Frase").innerHTML = '<p class="Zooming"><span class="Gradient">te amo!</span></p>'
+    document.getElementById("Frase").innerHTML = '<p class="Zooming"><span class="Gradient">te amo, infinitamente!</span></p>'
     document.querySelector(".bg").style.background = "url(https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwallpapercave.com%2Fwp%2FUQdBeGb.jpg&f=1&nofb=1) no-repeat;"
 }
 
